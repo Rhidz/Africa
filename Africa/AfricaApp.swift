176564bb7d404faf0982363e7cc337b31ee60dfc
Rhidita <rhidita20@gmail.com>
2021-04-14 23:07:10 +0600

    Initial Commit

---
+++ Africa/AfricaApp.swift
@@ -0,0 +1,17 @@
+//
+//  AfricaApp.swift
+//  Africa
+//
+//  Created by Admin on 14/04/2021.
+//
+
+import SwiftUI
+
+@main
+struct AfricaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
