c41284a32afe79fe70e813374ccdaf81e5f5a67a
Rhidita <36930197+Rhidz@users.noreply.github.com>
2021-04-26 21:49:10 +0600

    Update VideoPlayerHelper.swift

--- Africa/Helper/VideoPlayerHelper.swift
+++ Africa/Helper/VideoPlayerHelper.swift
@@ -6,8 +6,8 @@ import AVKit
 var videoPlayer: AVPlayer?
 
 func playVideo(fileName: String, fileFormat: String) -> AVPlayer {
-  if Bundle.main.url(forResource: fileName, withExtension: fileFormat) != nil {
-    videoPlayer = AVPlayer(url: Bundle.main.url(forResource: fileName, withExtension: fileFormat)!)
+  if let url = Bundle.main.url(forResource: fileName, withExtension: fileFormat) {
+    videoPlayer = AVPlayer(url: url)
     videoPlayer?.play()
   }
   return videoPlayer!
